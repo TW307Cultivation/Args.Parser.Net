30d0d96e1bdec81dd96042f654be503409026019
walle <walle@evy.null>
2018-03-14 21:41:15 +0800

    validate argument

--- src/Parser/ArgsParser.cs
+++ src/Parser/ArgsParser.cs
@@ -1,9 +1,29 @@
-﻿namespace Parser
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Parser.Tests;
+
+namespace Parser
 {
     public class ArgsParser
     {
         public string FullForm { get; set; }
         public char? AbbreviationForm { get; set; }
         public string Description { get; set; }
+
+        static readonly Regex FullFormRegex = new Regex(@"^--[a-zA-Z\d_]+[a-zA-Z\d_-]*$", RegexOptions.Compiled);
+        static readonly Regex AbbreviationFormRegex = new Regex(@"^-[a-zA-Z]{1}$", RegexOptions.Compiled);
+
+        public ArgsParsingResult Parse(string[] arguments)
+        {
+            foreach (var argument in arguments)
+            {
+                if (FullFormRegex.Match(argument).Success || AbbreviationFormRegex.Match(argument).Success)
+                {
+                    continue;
+                }
+                throw new ParserException($"Invalid argument format: {argument}");
+            }
+            return null;
+        }
     }
 }
--- test/Parser.Tests/ArgsParserTests.cs
+++ test/Parser.Tests/ArgsParserTests.cs
@@ -1,7 +1,28 @@
-﻿namespace Parser.Tests
+﻿using Xunit;
+
+namespace Parser.Tests
 {
     public class ArgsParserTests
     {
+        readonly ArgsParser parser;
 
+        public ArgsParserTests()
+        {
+            parser = new ArgsParserBuilder()
+                .AddFlagOption("flag", 'f', "this is description")
+                .Build();
+        }
+
+        [Theory]
+        [InlineData("-flag")]
+        [InlineData("---flag")]
+        [InlineData("f")]
+        void should_throw_exception_when_argument_invalid(string argument)
+        {
+            var exception = Assert.Throws<ParserException>(() => parser.Parse(new[] {argument}));
+
+            Assert.NotNull(exception);
+            Assert.Equal($"Invalid argument format: {argument}", exception.Message);
+        }
     }
 }
