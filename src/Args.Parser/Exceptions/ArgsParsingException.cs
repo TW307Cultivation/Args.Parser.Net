6de8d0ceebc9e7e9c0f1d3c7358ecb586321e13b
walle <walle@evy.null>
2018-03-18 21:13:40 +0800

    add document for ArgsParsingException

--- src/Args.Parser/Exceptions/ArgsParsingException.cs
+++ src/Args.Parser/Exceptions/ArgsParsingException.cs
@@ -4,18 +4,42 @@ using System.Collections.ObjectModel;
 
 namespace Args.Parser.Exceptions
 {
+    /// <summary>
+    /// Exception during parsing.
+    /// </summary>
     public class ArgsParsingException : Exception
     {
+        /// <summary>
+        /// The error type.
+        /// </summary>
         public ArgsParsingErrorCode Code { get; }
 
+        /// <summary>
+        /// The argument that cause error when parse.
+        /// </summary>
         public string Trigger { get; }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// Create <see cref="T:Args.Parser.Exceptions.ArgsParsingException" /> with
+        /// <paramref name="code" /> and <paramref name="trigger" />
+        /// </summary>
+        /// <param name="code">
+        /// The error type.
+        /// </param>
+        /// <param name="trigger">
+        /// The argument that cause error when parse.
+        /// </param>
         public ArgsParsingException(ArgsParsingErrorCode code, string trigger)
         {
             Code = code;
             Trigger = trigger;
         }
 
+        /// <inheritdoc />
+        /// <summary>
+        /// The error message built with error type.
+        /// </summary>
         public override string Message => messages[Code];
 
         readonly ReadOnlyDictionary<ArgsParsingErrorCode, string> messages =
