a6f2fa25f4c10833de0b526edff2d70cd0c97d98
walle <walle@evy.null>
2018-03-24 15:39:20 +0800

    change to const

--- src/Args.Parser/Models/Config.cs
+++ src/Args.Parser/Models/Config.cs
@@ -4,18 +4,16 @@ namespace Args.Parser.Models
 {
     static class Config
     {
-        internal static readonly Regex FullOptionRegex =
-            new Regex(@"^[a-zA-Z\d_]+[a-zA-Z\d_-]*$", RegexOptions.Compiled);
+        public static readonly Regex FullOptionRegex = new Regex(@"^[a-zA-Z\d_]+[a-zA-Z\d_-]*$", RegexOptions.Compiled);
 
-        internal static readonly Regex AbbrOptionRegex = new Regex(@"^[a-zA-Z]{1}$", RegexOptions.Compiled);
+        public static readonly Regex AbbrOptionRegex = new Regex(@"^[a-zA-Z]{1}$", RegexOptions.Compiled);
 
-        internal static readonly Regex
-            FullArgRegex = new Regex(@"^--[a-zA-Z\d_]+[a-zA-Z\d_-]*$", RegexOptions.Compiled);
+        public static readonly Regex FullArgRegex = new Regex(@"^--[a-zA-Z\d_]+[a-zA-Z\d_-]*$", RegexOptions.Compiled);
 
-        internal static readonly Regex AbbrArgRegex = new Regex(@"^-[a-zA-Z]{1}$", RegexOptions.Compiled);
+        public static readonly Regex AbbrArgRegex = new Regex(@"^-[a-zA-Z]{1}$", RegexOptions.Compiled);
 
-        internal static readonly string FullArgPrefix = "--";
+        public const string FullArgPrefix = "--";
 
-        internal static readonly string AbbrArgPrefix = "-";
+        public const string AbbrArgPrefix = "-";
     }
 }
